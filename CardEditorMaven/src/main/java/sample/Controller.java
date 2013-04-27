75365c17b2f9548081d7441a1a3adae2a77f1782
cdfeasy <cdf.easy@gmail.com>
2013-04-27 19:19:47 +0400

    111

--- CardEditorMaven/src/main/java/sample/Controller.java
+++ CardEditorMaven/src/main/java/sample/Controller.java
@@ -2,13 +2,33 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import zombies.entity.game.Card;
+import zombies.entity.support.*;
+
 
 public class Controller {
     @FXML
     public TextField id;
+    @FXML
+    public TableView<Card> CardList ;
+
     public void OnSearchClick(ActionEvent actionEvent) {
-        System.out.println("bla");
-        id.setText("blabla");
+        Session ses = zombies.entity.support.HibernateUtil.getSessionFactory().openSession();
+        ses.getTransaction().begin();
+        Query query = ses.createQuery("select card from Card card").setMaxResults(10);
+
+        for(Object obj: query.list()){
+            Card c=(Card)obj;
+            CardList.
+            CardList.getItems().add(c);
+        }
+        ses.getTransaction().commit();
+        ses.close();
+
     }
 }
---
+++ CardEditorMaven/src/main/java/sample/HibernateUtil.java
@@ -0,0 +1,36 @@
+package sample;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dmitry
+ * Date: 29.12.12
+ * Time: 19:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml)
+            // config file.
+            //  org.hibernate.cfg.
+            AnnotationConfiguration cfg=new AnnotationConfiguration().configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
+            sessionFactory = cfg.buildSessionFactory();
+
+
+        } catch (Throwable ex) {
+            // Log the exception.
+            System.err.println("Initial SessionFactory creation failed. " + ex);
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
